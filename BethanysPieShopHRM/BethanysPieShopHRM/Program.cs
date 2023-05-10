b42241caaceb6a1a30d7c7d06ce806d87df3ff94
AritroB <77176367+AritroB@users.noreply.github.com>
2023-05-10 16:57:09 -0400

    played around with variable types

--- BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Welcome to Bethany's Pie Shop HRM!");
 
+Console.WriteLine("Please enter your name: ");
+
+string name = Console.ReadLine();
+
+string name2 = Console.ReadLine();
+string name_2 = Console.ReadLine();
+
+//string 2Name = Console.Readline();
+
+int monthlyWage = 1234;
+
+int months = 12, bonus = 1000;
+
+bool isActive = true;
+
+double rating = 99.25;
+
+// byte numberOfEmployees = 300; does not work bc 300 is out of range of byte
+
+int hoursWorked;
+
+hoursWorked = 125;
+
+const double interestRate = 0.07;
+
+//interestRate = 0.08; doesn't work becaue of const
+
+string firstName = "Bethany";
+string lastName = "Smith";
+
+string emptyString = "";
+
+Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
