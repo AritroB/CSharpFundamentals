be03a7c14efd2903ccdd54eb1a502a3a1388028c
AritroB <77176367+AritroB@users.noreply.github.com>
2023-05-11 13:47:58 -0400

    finished working with methods

--- BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/BethanysPieShopHRM/Program.cs
@@ -4,6 +4,8 @@ using BethanysPieShopHRM;
 
 int amount = 1234;
 int months = 12;
+int bonus = 1000;
+Utilities.CalculateYearlyWage(amount, months, bonus);
 
 int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
 Console.WriteLine(yearlyWage);
